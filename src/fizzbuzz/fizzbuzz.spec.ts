16cda93f92cb589935a496def6cf043e486e0c51
Ana Jimenez <anajimenez.tw@packlink.com>
2020-11-28 16:52:14 +0100

    esqueleto fizzbuzz

---
+++ src/fizzbuzz/fizzbuzz.spec.ts
@@ -0,0 +1,9 @@
+import {generateFizzBuzz} from "./fizzbuzz";
+
+describe('Fizz Buzz', () => {
+
+    it('should return an string ', () => {
+        expect(generateFizzBuzz()).toBe('')
+    });
+
+})
---
+++ src/fizzbuzz/fizzbuzz.ts
@@ -0,0 +1,3 @@
+export const generateFizzBuzz = (): string => {
+return ''
+}
