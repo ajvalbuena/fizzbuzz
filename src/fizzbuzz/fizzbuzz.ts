bd696fa3a19b720b5f37b32ebcb13ff2d7d09748
Ana Jimenez <anajimenez.tw@packlink.com>
2020-11-28 17:19:27 +0100

    fizzBuzz for 1-5

--- src/fizzbuzz/fizzbuzz.ts
+++ src/fizzbuzz/fizzbuzz.ts
@@ -4,6 +4,7 @@ export const generateFizzBuzz = (init: number, final: number): string => {
         for (let i = init; i <= final; i++) {
             let nextNumber = i.toString()
             if(i==3){nextNumber='Fizz'}
+            if(i==5){nextNumber='Buzz'}
             fizzBuzzList = fizzBuzzList.concat(nextNumber.concat('\n'))
         }
 
