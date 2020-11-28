f7b8f88f7103758b4068f34fd0b644654ead7ac9
Ana Jimenez <anajimenez.tw@packlink.com>
2020-11-28 17:37:54 +0100

    extract getnext number method

--- src/fizzbuzz/fizzbuzz.ts
+++ src/fizzbuzz/fizzbuzz.ts
@@ -2,12 +2,24 @@ export const generateFizzBuzz = (init: number, final: number): string => {
     let fizzBuzzList = ''
 
         for (let i = init; i <= final; i++) {
-            let nextNumber = i.toString()
-            if(i==3 || i%3 == 0 || nextNumber.includes('3')){nextNumber='Fizz'}
-            if(i==5 || i%5 == 0 || nextNumber.includes('5')){nextNumber='Buzz'}
-            if(i%3 == 0 && i%5 == 0){nextNumber='FizzBuzz'}
+            let nextNumber = getNextNumber(i);
             fizzBuzzList = fizzBuzzList.concat(nextNumber.concat('\n'))
         }
 
     return fizzBuzzList
 }
+
+const getNextNumber = (i: number) => {
+    let nextNumber = i.toString()
+    if (i == 3 || i % 3 == 0 || nextNumber.includes('3')) {
+        nextNumber = 'Fizz'
+    }
+    if (i == 5 || i % 5 == 0 || nextNumber.includes('5')) {
+        nextNumber = 'Buzz'
+    }
+    if (i % 3 == 0 && i % 5 == 0) {
+        nextNumber = 'FizzBuzz'
+    }
+    return nextNumber;
+}
+
