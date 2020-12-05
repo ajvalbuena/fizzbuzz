d60d76f653488ff8ad19e9460274d9258a0d04b2
Ana Jimenez <anajimenez.tw@packlink.com>
2020-12-05 12:39:51 +0100

    adventOfCode: sum to 2020

--- src/adventofcode/sum2020.spec.ts
+++ src/adventofcode/sum2020.spec.ts
@@ -2,11 +2,11 @@ import {getDay1Challenge} from "./sum2020";
 
 describe('firstDayChallenge', () =>{
 
-    it("should find the two entries that sum to 5", () => {
+    it("should find the two entries that sum to 2020", () => {
 
-        const inputList: Array<number> = [1,2,3];
-        const expectedOutput = [2,3];
-        expect(getDay1Challenge(inputList)).toEqual((expectedOutput))
+        const inputList: Array<number> = [1721, 979, 366, 299, 675, 1456];
+        const expectedOutput = [1721,299];
+        expect(getDay1Challenge(inputList)).toEqual(expectedOutput)
 
     });
 })
--- src/adventofcode/sum2020.ts
+++ src/adventofcode/sum2020.ts
@@ -2,11 +2,11 @@ export const getDay1Challenge = (inputs: number[]): number[] => {
     let sumIs5List: Array<number> = [];
 
     inputs.map((input) =>{
-        let subarray = inputs.slice(inputs.indexOf(input)+1,inputs.length-1)
+        let subarray = inputs.slice(inputs.indexOf(input)+1,inputs.length)
         subarray.map((element) =>{
             let sum = input + element;
-            if(sum ==5){
-                return [input, element]
+            if(sum ==2020){
+                sumIs5List = [input, element]
             }
         })
     })
