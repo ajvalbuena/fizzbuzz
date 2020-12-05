fa2b9abc06918916a4170ef5407fd4bb84ee0a4e
Ana Jimenez <anajimenez.tw@packlink.com>
2020-12-05 12:34:25 +0100

    adventOfCode: using map rather than for

--- src/adventofcode/sum2020.ts
+++ src/adventofcode/sum2020.ts
@@ -2,13 +2,13 @@ export const getDay1Challenge = (inputs: number[]): number[] => {
     let sumIs5List: Array<number> = [];
 
     inputs.map((input) =>{
-        for(let i=0; i<=inputs.length; i++){
-            let sum = input + inputs[i];
+        let subarray = inputs.slice(inputs.indexOf(input)+1,inputs.length-1)
+        subarray.map((element) =>{
+            let sum = input + element;
             if(sum ==5){
-                return [input, inputs[i]]
+                return [input, element]
             }
-
-        }
+        })
     })
 
     return sumIs5List;
