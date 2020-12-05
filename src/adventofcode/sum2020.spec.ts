82845ea2db5f21fa3dbfff218021b5213cd8b855
Ana Jimenez <anajimenez.tw@packlink.com>
2020-12-05 17:30:29 +0100

    adventOfCode: multiply elements of the sum

--- src/adventofcode/sum2020.spec.ts
+++ src/adventofcode/sum2020.spec.ts
@@ -2,11 +2,18 @@ import {getDay1Challenge} from "./sum2020";
 
 describe('firstDayChallenge', () =>{
 
-    it("should find the two entries that sum to 2020", () => {
+    it("should return the multiplication of the two entries that sum to 2020", () => {
 
         const inputList: Array<number> = [1721, 979, 366, 299, 675, 1456];
-        const expectedOutput = [1721,299];
+        const expectedOutput = 514579;
         expect(getDay1Challenge(inputList)).toEqual(expectedOutput)
+
+    });
+
+    it("should return null if the elements do not satisfies the sum", () => {
+
+        const inputList: Array<number> = [1721, 979, 366, 300, 675, 1456];
+        expect(getDay1Challenge(inputList)).toEqual(undefined)
 
     });
 })
--- src/adventofcode/sum2020.ts
+++ src/adventofcode/sum2020.ts
@@ -1,15 +1,34 @@
-export const getDay1Challenge = (inputs: number[]): number[] => {
-    let sumIs5List: Array<number> = [];
+export const getDay1Challenge = (inputs: number[]): number | undefined => {
+    const sumArray: Array<number> = getArrayToSatisfiesTheSum(inputs, 2020);
 
-    inputs.map((input) =>{
-        let subarray = inputs.slice(inputs.indexOf(input)+1,inputs.length)
-        subarray.map((element) =>{
-            let sum = input + element;
-            if(sum ==2020){
-                sumIs5List = [input, element]
+    return multiplyArrayElements(sumArray);
+}
+
+
+function getArrayToSatisfiesTheSum(inputs: number[], totalSum: number) {
+    let sumArray: Array<number> = [];
+
+    inputs.map((number) => {
+        let subarray = inputs.slice(inputs.indexOf(number) + 1, inputs.length)
+        subarray.map((nextNumber) => {
+            let sum = number + nextNumber;
+            if (sum == totalSum) {
+                sumArray = [number, nextNumber]
             }
         })
     })
 
-    return sumIs5List;
+    return sumArray;
+}
+
+function multiplyArrayElements(inputs: Array<number>): number | undefined {
+    if (inputs.length == 0) {
+        return undefined
+    }
+
+    let arrayMultiplication: number = 1;
+    inputs.map((number) => {
+        arrayMultiplication *= number;
+    })
+    return arrayMultiplication;
 }
