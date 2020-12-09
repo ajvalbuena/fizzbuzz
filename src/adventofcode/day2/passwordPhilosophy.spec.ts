0e093e15713ceb5afda186a57648ca12e513eeab
Ana Jimenez <anajimenez.tw@packlink.com>
2020-12-09 19:02:47 +0100

    adventOfCode: returns number of times that the letter appears

--- src/adventofcode/day2/passwordPhilosophy.spec.ts
+++ src/adventofcode/day2/passwordPhilosophy.spec.ts
@@ -1,9 +1,9 @@
 import {passwordPhilosophy} from "./passwordPhilosophy";
 
 describe('secondDayChallenge - Password Philosophy - proof of concept', () => {
-    it('should return an string ', ()=>  {
-        const inputs = "1-3 a: abcde"
-        expect(passwordPhilosophy(inputs)).toBe(true)
+    it('should return the number of times that the letter appears in the password', ()=>  {
+        const inputs : Array<string>  = ["1-3 a: abcde"]
+        expect(passwordPhilosophy(inputs)).toBe(1)
 
     });
 })
--- src/adventofcode/day2/passwordPhilosophy.ts
+++ src/adventofcode/day2/passwordPhilosophy.ts
@@ -1,3 +1,18 @@
-export const passwordPhilosophy = (inputs: string[]) :boolean => {
-    return true;
+export const passwordPhilosophy = (inputs: string[]) :number => {
+    return getNumberOfTimesOfTheLetter("abcde", "a" );
+}
+
+
+const getNumberOfTimesOfTheLetter = (password: string, letter: string): number =>{
+    let numberOfTimes=0
+    for(let l of password){
+        if(l == letter){
+            numberOfTimes+= 1
+        }
+    }
+
+    if(password.startsWith(letter)){
+        return 1
+    }
+    return numberOfTimes
 }
