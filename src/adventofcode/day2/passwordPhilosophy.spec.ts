5f953865fbe584f6b7f46c26da365ff989cb0e74
Ana Jimenez <anajimenez.tw@packlink.com>
2020-12-09 19:31:13 +0100

    adventOfCode: passwordPhilosophy for one entry

--- src/adventofcode/day2/passwordPhilosophy.spec.ts
+++ src/adventofcode/day2/passwordPhilosophy.spec.ts
@@ -1,9 +1,15 @@
 import {passwordPhilosophy} from "./passwordPhilosophy";
 
 describe('secondDayChallenge - Password Philosophy - proof of concept', () => {
-    it('should return true for this letter and password', ()=>  {
+    it('should return 1 for one valid password in the array', ()=>  {
         const inputs : Array<string>  = ["1-3 a: abcde"]
-        expect(passwordPhilosophy(inputs)).toBe(true)
+        expect(passwordPhilosophy(inputs)).toBe(1)
+
+    });
+
+    it('should return 0 for none valid password in the array', ()=>  {
+        const inputs : Array<string>  = ["1-3 j: abcde"]
+        expect(passwordPhilosophy(inputs)).toBe(0)
 
     });
 })
--- src/adventofcode/day2/passwordPhilosophy.ts
+++ src/adventofcode/day2/passwordPhilosophy.ts
@@ -1,18 +1,31 @@
-export const passwordPhilosophy = (inputs: string[]) :boolean => {
-    return isValidPassword(3,1, getNumberOfTimesOfTheLetter("abcde", "a" ));
+export const passwordPhilosophy = (inputs: string[]): number => {
+    let numberOfValidPasswords = 0
+
+    const threeElements = separateElementsWithSeparator(inputs[0], " ")
+    const limits = separateElementsWithSeparator(threeElements[0], "-")
+    const letter = separateElementsWithSeparator(threeElements[1], ":")[0]
+    const password = threeElements[2]
+    const min = Number(limits [0])
+    const max = Number(limits[1])
+    numberOfValidPasswords += isValidPassword(max, min, getNumberOfTimesOfTheLetter(password, letter)) ? 1 : 0
+    return numberOfValidPasswords
 }
 
 
-const getNumberOfTimesOfTheLetter = (password: string, letter: string): number =>{
-    let numberOfTimes=0
-    for(let l of password){
-        if(l == letter){
-            numberOfTimes+= 1
+const getNumberOfTimesOfTheLetter = (password: string, letter: string): number => {
+    let numberOfTimes = 0
+    for (let l of password) {
+        if (l == letter) {
+            numberOfTimes += 1
         }
     }
     return numberOfTimes
 }
 
 const isValidPassword = (max: number, min: number, numberOfTimes: number): boolean => {
-    return (min<= numberOfTimes && numberOfTimes <= max)
+    return (min <= numberOfTimes && numberOfTimes <= max)
+}
+
+const separateElementsWithSeparator = (input: string, separator: string): Array<string> => {
+    return input.split(separator)
 }
