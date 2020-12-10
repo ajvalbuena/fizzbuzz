54b0da81387791fee60f35afd30f4f76085043ba
Ana Jimenez <anajimenez.tw@packlink.com>
2020-12-10 09:57:59 +0100

    adventOfCode: refactor

--- src/adventofcode/day2/passwordPhilosophy.ts
+++ src/adventofcode/day2/passwordPhilosophy.ts
@@ -1,18 +1,12 @@
 export const passwordPhilosophy = (inputs: string[]): number => {
     let numberOfValidPasswords = 0
-
-    const threeElements = separateElementsWithSeparator(inputs[0], " ")
-    const limits = separateElementsWithSeparator(threeElements[0], "-")
-    const letter = separateElementsWithSeparator(threeElements[1], ":")[0]
-    const password = threeElements[2]
-    const min = Number(limits [0])
-    const max = Number(limits[1])
-    numberOfValidPasswords += isValidPassword(max, min, getNumberOfTimesOfTheLetter(password, letter)) ? 1 : 0
+    const {letter, password, min, max} = getElementsOfTheInputs(inputs);
+    numberOfValidPasswords += isaValidPassword(max, min, getNumberOfTimesTheLetterIsRepeated(password, letter)) ? 1 : 0
     return numberOfValidPasswords
 }
 
 
-const getNumberOfTimesOfTheLetter = (password: string, letter: string): number => {
+const getNumberOfTimesTheLetterIsRepeated = (password: string, letter: string): number => {
     let numberOfTimes = 0
     for (let l of password) {
         if (l == letter) {
@@ -22,10 +16,16 @@ const getNumberOfTimesOfTheLetter = (password: string, letter: string): number =
     return numberOfTimes
 }
 
-const isValidPassword = (max: number, min: number, numberOfTimes: number): boolean => {
-    return (min <= numberOfTimes && numberOfTimes <= max)
-}
+const isaValidPassword = (max: number, min: number, numberOfTimes: number): boolean => (min <= numberOfTimes) && (numberOfTimes <= max)
 
-const separateElementsWithSeparator = (input: string, separator: string): Array<string> => {
-    return input.split(separator)
+const separateElementsWithSeparator = (input: string, separator: string): Array<string> =>  input.split(separator)
+
+function getElementsOfTheInputs(inputs: string[]) {
+    const threeElements = separateElementsWithSeparator(inputs[0], " ")
+    const limits = separateElementsWithSeparator(threeElements[0], "-")
+    const letter = separateElementsWithSeparator(threeElements[1], ":")[0]
+    const password = threeElements[2]
+    const min = Number(limits [0])
+    const max = Number(limits[1])
+    return {letter, password, min, max};
 }
