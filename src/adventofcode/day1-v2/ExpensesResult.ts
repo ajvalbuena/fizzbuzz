d7776a78c2d2dab10462c217cbc1c97f0e7050b5
Ana Jimenez <anajimenez.tw@packlink.com>
2021-01-30 13:14:31 +0100

    adventOfCode: add elements and methods to the class

--- src/adventofcode/day1-v2/ExpensesResult.ts
+++ src/adventofcode/day1-v2/ExpensesResult.ts
@@ -7,19 +7,53 @@ export class ExpensesResult {
     }
 
 
-   public sumOfNElements = () => this.operators.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
-   private multiplicationOfNElements = (elements: number[]) => elements.reduce((accumulator, currentValue) => accumulator * currentValue, 1)
+    sumOfNElements = () => this.operators.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    private multiplicationOfNElements = (elements: number[]) => elements.reduce((accumulator, currentValue) => accumulator * currentValue, 1)
 
-   multiplication(found: boolean) {
-        let result = undefined
-        if (found) {
-            result = this.multiplicationOfNElements(this.operators)
+    found = (sum: number = 2020): boolean => this.sumOfNElements() == sum;
+    multiplication = (found: boolean) => found ?  this.multiplicationOfNElements(this.operators) : undefined;
+
+
+    elementsThatSatisfyTheSum = (sum: number, i: number, expenses: Array<number>) => {
+        while (!this.found(sum) && i <= expenses.length) {
+            this.findingElementsToSatisfyTheSum(i, sum, expenses);
+            i++
         }
-        return result;
+        return new ExpensesResult(this.operators)
+    }
+
+    threeElementsThatSatisfyTheSum = (sum: number, i: number, expenses: Array<number>) => {
+        while (!this.found(sum) && i <= expenses.length) {
+            let j = i + 1
+            while (!this.found(sum) && j <= expenses.length) {
+                this.finding3ElementsToSatisfyTheSum(i, j, sum, expenses);
+                j++
+            }
+            i++
+        }
+        return new ExpensesResult(this.operators)
     }
 
 
-    found= (sum: number = 2020): boolean => this.sumOfNElements() == sum;
 
+
+    findingElementsToSatisfyTheSum = (i: number, sum: number, expenses: Array<number>) => {
+        this.operators = Array()
+        let j = i + 1
+        while (!this.found(sum) && j <= expenses.length) {
+            this.operators = Array(expenses[i], expenses[j])
+            j++
+        }
+    }
+
+
+    finding3ElementsToSatisfyTheSum = (i: number, j: number, sum: number, expenses: Array<number>) => {
+        this.operators = Array()
+        let x = j+1
+        while (!this.found(sum) && x <= expenses.length) {
+            this.operators = Array(expenses[i], expenses[j], expenses[x])
+            x++
+        }
+    }
 
 }
--- src/adventofcode/day1-v2/reportRepair.ts
+++ src/adventofcode/day1-v2/reportRepair.ts
@@ -1,48 +1,18 @@
 import {ExpensesResult} from "./ExpensesResult";
 
-function calculateResult2(i: number, sum: number, expenses: Array<number>): ExpensesResult{
-    let expensesResult = new ExpensesResult(Array())
-    let j = i + 1
-    while (!expensesResult.found(sum) && j <= expenses.length) {
-        expensesResult = new ExpensesResult(Array(expenses[i], expenses[j]))
-        j++
-    }
-    return expensesResult;
-}
-
-function calculateResult3(i: number, j: number, sum: number, expenses: Array<number>): ExpensesResult{
-    let expensesResult = new ExpensesResult(Array())
-    let x = j+1
-    while (!expensesResult.found(sum) && x <= expenses.length) {
-        expensesResult = new ExpensesResult(Array(expenses[i], expenses[j], expenses[x]))
-        x++
-    }
-    return expensesResult;
-}
 
 export const reportRepair = (expenses: Array<number>, sum: number): number | undefined => {
     let expensesResult = new ExpensesResult(Array())
     let i = 0
-    while (!expensesResult.found(sum) && i <= expenses.length) {
-        expensesResult = calculateResult2(i, sum, expenses);
-        i++
-    }
-
+    expensesResult = expensesResult.elementsThatSatisfyTheSum(sum, i, expenses);
     return expensesResult.multiplication(expensesResult.found(sum));
 
 }
+
 
 export const reportRepair3 = (expenses: Array<number>, sum: number): number | undefined => {
     let expensesResult = new ExpensesResult(Array())
     let i = 0
-    while (!expensesResult.found(sum)&& i <= expenses.length) {
-        let j = i+1
-        while (!expensesResult.found(sum) && j <= expenses.length) {
-            expensesResult = calculateResult3(i, j, sum, expenses);
-            j++
-        }
-        i++
-    }
+    expensesResult = expensesResult.threeElementsThatSatisfyTheSum( sum, i, expenses);
     return expensesResult.multiplication(expensesResult.found(sum));
 }
-
